9ee6a15ed34a182d594dd1e99b87d01df8b439a6
Jeong Arm <kjwonmail@gmail.com>
2023-06-15 01:00:16 +0900

    Remove debugging option for uvicorn

--- start.sh
+++ start.sh
@@ -5,4 +5,4 @@ cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
 source .venv/bin/activate
 
-exec uvicorn --host 0.0.0.0 --port 8000 --reload --log-level info app:app
+exec uvicorn --host 0.0.0.0 --port 8000 --log-level info app:app
